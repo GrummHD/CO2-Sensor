67033c05f62b438e40e2a3917e1edc8a412fcfb8
Jasper-Sic <Jasperda77@yahoo.de>
2020-01-14 17:29:50 +0100

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -1,10 +1,10 @@
 // Use this file to store all of the private credentials 
 // and connection details
 
-#define SECRET_SSID "CO2"    // replace MySSID with your WiFi network name
-#define SECRET_PASS "JasperSickinger"  // replace MyPassword with your WiFi password
-#define SECRET_CH_ID 929617			// replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "C8YOTKOV0QNK92Z1"   // replace XYZ with your channel write API Key
+#define SECRET_SSID "MySSID"    // replace MySSID with your WiFi network name
+#define SECRET_PASS "MyPassword"  // replace MyPassword with your WiFi password
+#define SECRET_CH_ID 0000000			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "XYZ"   // replace XYZ with your channel write API Key
 
 /*  Raum 70 929617 C8YOTKOV0QNK92Z1
 Raum 46 929543 6NPBBLNC60F94VOR
